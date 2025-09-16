caaa9abb2426d5c1b9435381236e24e7a64feb9e
Mirandosky-12 <147409476+Mirandosky-12@users.noreply.github.com>
2025-09-15 19:46:46 -0500

    Lab2 Listo

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.FirstName = "Diego";
+            client.LastName = "Lopez";
+            client.Age = 15;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+}
---
+++ Laboratorio2/Laboratorio21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboratorio21
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //.
+            MyClass.valor = 1;
+            Console.WriteLine(MyClass.valor);
+        }
+    }
+
+    public class MyClass
+    {
+        public static int valor;
+    }
+}
