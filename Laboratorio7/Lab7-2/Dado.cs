48867094eecfe55e0c45c05cee3445a632594fa7
Mirandosky-12 <147409476+Mirandosky-12@users.noreply.github.com>
2025-09-24 18:36:45 -0500

    Laboratorio 7 Listo

---
+++ Laboratorio7/Lab7-2/Dado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab7_2
+{
+    class Dado
+    {
+        private int valor;
+        private static Random aleatorio;
+
+        public Dado()
+        {
+            aleatorio = new Random();
+        }
+
+        public void Tirar()
+        {
+            valor = aleatorio.Next(1, 7);
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("El valor del dado es: " + valor);
+        }
+
+        public int RetornarValor()
+        {
+            return valor;
+        }
+    }
+}
---
+++ Laboratorio7/Laboratorio7/Program.cs
@@ -0,0 +1,13 @@
+﻿using Laboratorio7;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Banco banco1 = new Banco();
+        banco1.Operar();
+        banco1.DepositarTotales();
+        Console.ReadKey();
+    }
+}
