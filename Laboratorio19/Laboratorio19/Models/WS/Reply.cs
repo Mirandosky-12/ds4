69ddb17035bac333e999be9af1d09aa98b4a5503
Mirandosky-12 <147409476+Mirandosky-12@users.noreply.github.com>
2025-11-16 21:26:40 -0500

    Lab19 completo

---
+++ Laboratorio19/Laboratorio19/Models/WS/Reply.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio19.Models.WS
+{
+    public class Reply
+    {
+        internal int result;
+        internal string message;
+    }
+}
