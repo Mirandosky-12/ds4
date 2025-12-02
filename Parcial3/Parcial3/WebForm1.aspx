956045fc7461b669a481f4ff8fdb9da54d958c5a
Mirandosky-12 <147409476+Mirandosky-12@users.noreply.github.com>
2025-12-01 19:56:12 -0500

    Parcial #3

---
+++ Parcial3/Parcial3/WebForm1.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="GestionCasosLegales.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Gestión de Casos Legales</title>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="main-container">
+            <h2>Registro de Caso Legal</h2>
+            
+
+            <div class="row">
+                <div class="col form-group">
+                    <label>Título del Caso:</label>
+                    <asp:TextBox ID="txtTitulo" runat="server" CssClass="input-field" placeholder="Ej: Demanda X vs Y"></asp:TextBox>
+                </div>
+                <div class="col form-group">
+                    <label>Cliente:</label>
+                    <asp:TextBox ID="txtCliente" runat="server" CssClass="input-field" placeholder="Nombre del cliente"></asp:TextBox>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col form-group">
+                    <label>Abogado Asignado:</label>
+                    <asp:DropDownList ID="ddlAbogado" runat="server" CssClass="input-field">
+                        <asp:ListItem Text="-- Seleccione --" Value="" />
+                        <asp:ListItem Text="Lic. Santiago" Value="Lic. Santiago" />
+                        <asp:ListItem Text="Lic. Herrera" Value="Lic. Herrera" />
+                        <asp:ListItem Text="Lic. Gomez" Value="Lic. Gomez" />
+                    </asp:DropDownList>
+                </div>
+                <div class="col form-group">
+                    <label>Estado del Caso:</label>
+                    <asp:DropDownList ID="ddlEstado" runat="server" CssClass="input-field">
+                        <asp:ListItem Text="Nuevo" Value="Nuevo" />
+                        <asp:ListItem Text="En Proceso" Value="En Proceso" />
+                        <asp:ListItem Text="En Espera" Value="En Espera" />
+                        <asp:ListItem Text="Cerrado" Value="Cerrado" />
+                    </asp:DropDownList>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col form-group">
+                    <label>Fecha de Inicio:</label>
+                    <asp:TextBox ID="txtFechaInicio" runat="server" CssClass="input-field" TextMode="Date"></asp:TextBox>
+                </div>
+                <div class="col form-group">
+                    <label>Fecha de Vencimiento:</label>
+                    <asp:TextBox ID="txtFechaVencimiento" runat="server" CssClass="input-field" TextMode="Date"></asp:TextBox>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label>Descripción / Notas:</label>
+                <asp:TextBox ID="txtDescripcion" runat="server" CssClass="input-field" TextMode="MultiLine" Rows="3"></asp:TextBox>
+            </div>
+
+            <asp:Button ID="btnRegistrar" runat="server" Text="Registrar Caso" CssClass="btn" OnClick="btnRegistrar_Click" />
+            
+            <asp:Label ID="lblMensaje" runat="server" CssClass="message"></asp:Label>
+
+            <hr />
+
+            <h3>Casos Registrados</h3>
+            <asp:GridView ID="gvCasos" runat="server" CssClass="grid-view" AutoGenerateColumns="true" EmptyDataText="No hay casos registrados.">
+            </asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Parcial3/Parcial3/WebForm1.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient; 
+
+namespace GestionCasosLegales
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        
+        string connectionString = "Data Source=.;Initial Catalog=DiegoLopez;Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarCasos();
+            }
+        }
+
+        protected void btnRegistrar_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtTitulo.Text) || string.IsNullOrWhiteSpace(txtCliente.Text))
+            {
+                lblMensaje.Text = "Por favor complete los campos obligatorios.";
+                lblMensaje.CssClass = "message error";
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+
+                    string query = "INSERT INTO DL_CasosLegales (TituloCaso, NombreCliente, AbogadoAsignado, Estado, FechaInicio, FechaVencimiento, Descripcion) " +
+                                   "VALUES (@Titulo, @Cliente, @Abogado, @Estado, @Inicio, @Vence, @Desc)";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@Titulo", txtTitulo.Text);
+                        cmd.Parameters.AddWithValue("@Cliente", txtCliente.Text);
+                        cmd.Parameters.AddWithValue("@Abogado", ddlAbogado.SelectedValue);
+                        cmd.Parameters.AddWithValue("@Estado", ddlEstado.SelectedValue);
+                        cmd.Parameters.AddWithValue("@Inicio", DateTime.Parse(txtFechaInicio.Text));
+
+                        if (string.IsNullOrEmpty(txtFechaVencimiento.Text))
+                            cmd.Parameters.AddWithValue("@Vence", DBNull.Value);
+                        else
+                            cmd.Parameters.AddWithValue("@Vence", DateTime.Parse(txtFechaVencimiento.Text));
+
+                        cmd.Parameters.AddWithValue("@Desc", txtDescripcion.Text);
+
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+
+                lblMensaje.Text = "¡Caso registrado exitosamente!";
+                lblMensaje.CssClass = "message success";
+                LimpiarCampos();
+                CargarCasos();
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "Error al registrar: " + ex.Message;
+                lblMensaje.CssClass = "message error";
+            }
+        }
+
+        private void CargarCasos()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM DL_CasosLegales ORDER BY FechaInicio DESC";
+                    SqlDataAdapter da = new SqlDataAdapter(query, con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    gvCasos.DataSource = dt;
+                    gvCasos.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "Error al cargar datos: " + ex.Message;
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            txtTitulo.Text = "";
+            txtCliente.Text = "";
+            txtDescripcion.Text = "";
+            ddlAbogado.SelectedIndex = 0;
+            ddlEstado.SelectedIndex = 0;
+            txtFechaInicio.Text = "";
+            txtFechaVencimiento.Text = "";
+        }
+    }
+}
